a3046743769a57bda5d98b156a6a55f97ddadcce
Christophe03 <81380467+Christophe03@users.noreply.github.com>
2021-11-15 21:49:03 +0000

    Admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TypeComponent } from './type/type.component';
+import { LoginComponent } from './login/login.component';
+import { AjoutComponent } from './ajout/ajout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypeComponent,
+    LoginComponent,
+    AjoutComponent
   ],
   imports: [
     BrowserModule,
